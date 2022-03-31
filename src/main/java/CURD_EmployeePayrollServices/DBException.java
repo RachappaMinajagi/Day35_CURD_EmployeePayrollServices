d3c600d0a180ec413c2825d31b9cd5f4d6451048
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-03-31 17:48:03 +0530

    Added:Retrive Employee Payroll DataFrom Database

---
+++ src/main/java/CURD_EmployeePayrollServices/DBException.java
@@ -0,0 +1,14 @@
+package CURD_EmployeePayrollServices;
+
+public class DBException extends Exception {
+
+	public enum ExceptionType{
+		CONNECTION_FAIL
+	}
+	ExceptionType type;
+
+	public DBException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
